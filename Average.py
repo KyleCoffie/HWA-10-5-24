de9fa5bd56ba1237dc593843bfd7b174df004049
KyleCoffie <160148467+KyleCoffie@users.noreply.github.com>
2024-10-05 17:32:41 -0500

    first commit

---
+++ Average.py
@@ -0,0 +1,8 @@
+#Task 2: Calculate the average grade and print it.
+
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+
+total_of_grades = sum(grades)
+number_of_grades =len(grades)    
+
+print(total_of_grades/number_of_grades)
